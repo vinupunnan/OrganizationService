0d640cdada7c2f9317d3663b18958e9535f3a7a5
account <account>
2020-01-06 16:34:39 -0500

    change in url for employee

--- src/main/java/com/organization/api/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/organization/api/service/impl/OrganizationServiceImpl.java
@@ -21,7 +21,7 @@ public class OrganizationServiceImpl implements OrganizationService{
 	@Override
 	public List<OrgEmpDeptDetails> callDataSources() {
 		
-		EmployeeList employeeList = restTemplate.getForObject("http://localhost:8081/employee",EmployeeList.class );
+		EmployeeList employeeList = restTemplate.getForObject("http://localhost:8081/employees",EmployeeList.class );
 		return employeeList.getEmployees().stream().map(employee ->{
 		Department dept =	restTemplate.getForObject("http://localhost:8082/depts/" +employee.getDepId(),Department.class);
 		return new OrgEmpDeptDetails(employee.getEmpName(),employee.getEmpDesignation(),dept.getDeptName(),dept.getDeptLocation());
